c7fe02dae4f54191d2993ffda48b83c5b56ac078
pepsi <pepsi@localhost.localdomain>
2024-10-24 13:50:21 +0200

    había mezclado los nombres de estas dos jaja

--- Leantest/TopoSpaces/trivial.lean
+++ Leantest/TopoSpaces/trivial.lean
@@ -1,17 +1,66 @@
-import Mathlib.Tactic
+import Mathlib.Tactic -- catarme los imports necesarios solo
 
--- ex 1
--- prove that the trivial top. space is in fact a top
+-- import Mathlib.Topology.Basic
+-- me falla classical em
+
+-- ex 2
+-- prove that the discrete top. space is in fact a top
+
+
+/-
+PROBLEMA: me gustaría separar esto en lemmas
+pero me da problemas con los tipos...
+-/
+
 
 open TopologicalSpace
 
 def TrivialTopology (X : Type) : TopologicalSpace X where
-  IsOpen (_ : Set X) := true
+  IsOpen (s : Set X) := s = Set.univ ∨ s = ∅
   isOpen_univ := by
-    trivial
+    dsimp -- DEFINITIONAL SIMPLIFIER
+    left
+    rfl -- or trivial
+
   isOpen_inter := by
-    intros
-    trivial
+    dsimp
+    intro s t hs ht
+
+    cases' hs with hs hs
+    cases' ht with ht ht
+
+    · left -- 1. both are univ
+      rw [hs, ht]
+      simp
+    · right -- 2. t is empty
+      rw [ht]
+      simp
+    · right -- 3. s is empty
+      rw [hs]
+      simp
+
   isOpen_sUnion := by
-    intros
-    trivial
+    dsimp
+    intro S hS
+
+    have h : (Set.univ ∈ S) ∨ ¬ (Set.univ ∈ S)
+    exact Classical.em (Set.univ ∈ S)
+
+    cases' h with h h
+
+    · left -- 1. ∃ t ∈ S with t = U
+      ext s
+      constructor <;> intro hs
+      · trivial
+      · simp
+        use Set.univ
+
+    · right
+      simp
+      intro s hs
+      specialize hS s hs
+      cases' hS with hS hS
+      · by_contra
+        rw [hS] at hs
+        exact h hs
+      · exact hS
