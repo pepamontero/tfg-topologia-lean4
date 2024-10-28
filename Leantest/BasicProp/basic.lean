1b650d668e3aed448d854b98a86ffffaa96c916a
pepsi <pepsi@localhost.localdomain>
2024-10-28 18:01:50 +0100

    added basic neighbourhood concepts

--- Leantest/BasicProp/basic.lean
+++ Leantest/BasicProp/basic.lean
@@ -2,7 +2,6 @@ import Mathlib.Tactic
 
 /-
 Demostrar los siguientes resultados:
-
 -/
 
 #check isOpen_empty
@@ -14,3 +13,22 @@ example (X : Type) [TopologicalSpace X] : IsOpen (∅ : Set X) := by
   intro t ht
   by_contra
   exact ht
+
+open TopologicalSpace
+
+
+
+def OpenNeighbourhood {X : Type} [TopologicalSpace X]
+    (U : Set X) (x : X) : Prop :=
+    x ∈ U ∧ IsOpen U
+
+def Neighbourhood {X : Type} [TopologicalSpace X]
+    (V : Set X) (x : X) : Prop :=
+    ∃ U : Set X, U ⊆ V ∧ OpenNeighbourhood U x
+
+example {X : Type} [TopologicalSpace X]
+    (U : Set X) (x : X) : OpenNeighbourhood U x →
+    Neighbourhood U x := by
+  intro hU
+  rw [Neighbourhood]
+  use U
