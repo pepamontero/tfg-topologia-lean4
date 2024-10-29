dbc8d2a1005eb72cf730b46e6bcbe6baaff0770a
pepsi <pepsi@localhost.localdomain>
2024-10-29 13:59:51 +0100

    almost done with hausdorff

--- Leantest/Separation/hausdorff.lean
+++ Leantest/Separation/hausdorff.lean
@@ -2,6 +2,7 @@ import Leantest.BasicProp.basic
 import Leantest.TopoSpaces.discrete
 import Leantest.TopoSpaces.trivial
 import Leantest.TopoSpaces.usual
+import Leantest.TopoSpaces.point
 
 #check Neighbourhood
 
@@ -252,3 +253,183 @@ example [T : TopologicalSpace ℝ] (hT : T = UsualTopology) : Hausdorff T := by
           exact hδ'
         · apply is_open_open_interval hT (x2 - δ) (x2 + δ)
           linarith
+
+/-
+RESULTADO
+Si X es un espacio topológico Hausdorff,
+entonces todo conjunto unipuntual {x} es cerrado
+-/
+
+#check IsClosed
+
+example {X : Type} (A : Set X) (hA : ¬ (∃ a, a ∈ A)) : A = ∅ := by
+  exact Set.not_nonempty_iff_eq_empty.mp hA
+
+
+example {X : Type} (A : Set X) (hA : ¬ (A = ∅)) : ∃ a, a ∈ A := by
+  by_contra
+
+lemma A_open_iff_is_heighbourhood_of_all
+    {X : Type} [T : TopologicalSpace X]
+    (A : Set X) : IsOpen A ↔
+    ∀ x ∈ A, Neighbourhood A x := by
+  constructor
+  all_goals intro h
+  · -- →
+    intro x hx
+    use A
+    constructor
+    trivial
+    constructor
+    exact hx
+    exact h
+  · -- ←
+    -- classeical choose?
+    have h' : ∀ x ∈ A, ∃ Ux : Set X, (Ux ⊆ A ∧ OpenNeighbourhood Ux x)
+    · exact h
+
+    have aux : A = ∅ ∨ ¬ (A = ∅)
+    exact Classical.em (A = ∅)
+
+    cases' aux with h1 h2
+
+    · -- A is empty
+      rw [h1]
+      exact isOpen_empty
+
+    · -- A is not empty
+      have hA : ∃ a, a ∈ A
+      · rw [← Set.not_nonempty_iff_eq_empty] at h2
+        simp at h2
+        exact h2
+
+      let g : A → Set X := fun a : A ↦ Classical.choose h' a
+
+      let a := Classical.choose hA
+      let ha := Classical.choose_spec hA
+      specialize h' a ha
+      sorry
+
+
+
+
+theorem Hausdorff_imp_singletones_closed {X : Type} [T : TopologicalSpace X]
+    (h : Hausdorff T) : ∀ x : X, IsClosed {x} := by
+
+  intro x
+  have h' : IsOpen {x}ᶜ
+  · rw [A_open_iff_is_heighbourhood_of_all]
+    intro y hy
+    have hxy : x ≠ y
+    · by_contra c
+      rw [c] at hy
+      apply hy
+      trivial
+    specialize h x y hxy
+    cases' h with V1 h
+    cases' h with V2 h
+    cases' h with h h1
+    cases' h1 with h1 h2
+    cases' h1 with U1 h1
+    cases' h2 with U2 h2
+    use U2
+    constructor
+    · simp
+      have g1 : x ∈ V1
+      · apply h1.left
+        exact h1.right.left
+      have g2 : x ∉ V2
+      · by_contra hx
+        have hx' : x ∈ V1 ∩ V2
+        · constructor; exact g1; exact hx
+        rw [h] at hx'
+        exact hx'
+      by_contra hx
+      apply h2.left at hx
+      exact g2 hx
+    · exact h2.right
+
+
+  exact { isOpen_compl := h' }
+
+
+/-
+Ejemplo de topología que no es Hausdorff
+La topología del punto para conjuntos de 2 o más elementos
+-/
+
+-- usando la definición
+
+lemma and_or_not (P Q : Prop) : (P ∨ Q) ∧ ¬ Q → P := by
+  intro hP
+  cases' hP with h1 h2
+  cases' h1 with h h
+  exact h
+  by_contra
+  exact h2 h
+
+example [T : TopologicalSpace X] (a : X) (h : ∃ x : X, x ≠ a)
+    (hT : T = PointTopology X a) : ¬ Hausdorff T := by
+  cases' h with x hx
+  by_contra h
+  specialize h x a hx
+  cases' h with Vx h
+  cases' h with Va h
+  cases' h with h hx
+  cases' hx with hx ha
+  cases' hx with Ux hx
+  cases' hx with hx1 hx2
+  cases' hx2 with hx2 hx3
+  have ha1 : a ∈ Ux
+  · rw [hT] at hx3
+    have aux : Ux ≠ ∅
+    · by_contra aux
+      rw [aux] at hx2
+      exact hx2
+    apply and_or_not (a ∈ Ux) (Ux = ∅)
+    constructor
+    exact hx3
+    exact aux
+  have ha2 : ¬ a ∈ Ux
+  · by_contra
+    have c : a ∈ Vx ∩ Va
+    · constructor
+      · apply hx1
+        exact ha1
+      · cases' ha with Ua ha
+        apply ha.left
+        exact ha.right.left
+    rw [h] at c
+    exact c
+  exact ha2 ha1
+
+#check isClosed_compl_iff
+#check compl_compl
+
+-- usando el resultado anterior
+example [T : TopologicalSpace X] (a : X) (hX : ∃ x : X, x ≠ a)
+    (hT : T = PointTopology X a) : ¬ Hausdorff T := by
+  by_contra h
+  apply Hausdorff_imp_singletones_closed at h
+  specialize h a
+
+  let U : Set X := {a}
+  have hU : U = {a} := by rfl
+  rw [← hU] at h
+
+  rw [← compl_compl U] at h
+  rw [isClosed_compl_iff] at h
+  rw [hT] at h
+  cases' h with h1 h2
+  · have c : a ∉ Uᶜ
+    · rw [hU]
+      trivial
+    exact c h1
+  · cases' hX with x hx
+    have c : x ∈ Uᶜ
+    · simp
+      rw [hU]
+      simp
+      exact hx
+    rw [h2] at c
+    exact c
