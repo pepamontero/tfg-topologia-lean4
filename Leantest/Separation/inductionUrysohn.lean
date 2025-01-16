0afdce5e5ed7ee842c4fa735738be4daee2dc53a
pepsi <pepamonterojimena@gmail.com>
2025-01-16 15:40:33 +0100

    me he dado cuenta de que no estoy sacando bien el p y el q. lo dejo asi pero voy con otra idea

--- Leantest/Separation/inductionUrysohn.lean
+++ Leantest/Separation/inductionUrysohn.lean
@@ -159,56 +159,167 @@ example {X : Type}
 
       let i : ℕ := Finset.max' I H
 
+      sorry
+
+
+example {X : Type}
+    (T : TopologicalSpace X)
+    (hT : NormalTopoSpace T)
+    (C1 C2 : Set X)
+    (hC1 : IsClosed C1)
+    (hC2 : IsClosed C2)
+
+    (f : ℕ → ℚ)
+
+    (hf0 : f 0 = 1)
+    (hf1 : f 1 = 0)
+
+    (hf : ∀ n : ℕ, 0 ≤ f n ∧ f n ≤ 1)
+    (finy : f.Injective)
+
+    (P : Finset ℕ)
+
+    (hP : { n : ℕ | (∃ U : Set X, IsOpen U ∧
+    (∀ m ∈ P, ∃ V : Set X, (IsOpen V) ∧ (f n < f m → Closure (U) ⊆ V))) })
+
+    /-
+    es que esto no me convence nada porque
+    la elección de V depende de la elección de U...
+    y no se si eso luego se puede salvar a la
+    hora de definir G...
+    -/
+
+    :
+
+    True := by
+  sorry
 
 
 
+example {X : Type}
+    (T : TopologicalSpace X)
+    (hT : NormalTopoSpace T)
+    (C1 C2 : Set X)
+    (hC1 : IsClosed C1)
+    (hC2 : IsClosed C2)
+
+    (G : ℚ → Set X)
+    (f : ℕ → ℚ)
+
+    (hf0 : f 0 = 1)
+    (hf1 : f 1 = 0)
+
+    (hf : ∀ n : ℕ, 0 ≤ f n ∧ f n ≤ 1)
+    (finy : f.Injective)
+
+    (P : Finset ℕ)
+
+    (hP : { n : ℕ | IsOpen (G (f n)) ∧
+    (∀ m ∈ P, f n < f m → Closure (G (f n)) ⊆ G (f m)) })
 
 
+    (h0 : 0 ∈ P)
+    (h1 : 1 ∈ P)
+
+    --(k : ℕ) (hk : k = P.card)
+
+    (n : ℕ)
+
+    (H : ℕ → Prop)
+    (hH : H = fun n ↦ (n = 0) ∨ (n = 1) ∨ (∃ s ∈ P, (f s < f n ∧ (∀ m ∈ P, f m < f n → f m ≤ f s))))
+
+    :
+
+    H n := by
+
+  apply Nat.strong_induction_on
+  intro n hn
+
+  have Hdef : H n = ((n = 0) ∨ (n = 1) ∨ (∃ s ∈ P, (f s < f n ∧ (∀ m ∈ P, f m < f n → f m ≤ f s))))
+  exact congrFun hH n
+
+  rw [Hdef]
 
 
+  /-
+  paso 1. Quiero ver que n no es 0 ni 1
+  (seguramente hay formas más fáciles de hacerlo)
+  -/
 
-      have hS : S ⊆ P
-      intro x hx
-      simp [Sdef] at hx
-      exact hx.left
+  have aux : n = 0 ∨ n > 0
+  exact Nat.eq_zero_or_pos n
+  cases' aux with hn' hn'
 
-      have hS : Finset S
-      exact Finset.subtype (fun x ↦ x ∈ S) P
+  · -- n=0
+    rw [hn']
+    left
+    rfl
 
-      let s : ℕ := Min S'
+  · have aux : n = 1 ∨ n > 1
+    exact LE.le.eq_or_gt hn'
+    cases' aux with hn' hn'
 
+    · -- n=1
+      rw [hn']
+      right
+      left
+      rfl
 
+    · -- n not 0 or 1
 
-      have aux : f n ≠ 0
-      by_contra c
-      rw [← hf1] at c
-      apply finy at c
-      linarith
+  /-
+  paso 2. si n no es 0 ni 1,
+  entonces f n no es 0 ni 1 (f inyectiva)
+  entonces puedo encontrar t,s en P tal que
+  f t < f n < f s
+  -/
 
-      have aux' : f n ≠ 1
-      by_contra c
-      rw [← hf0] at c
-      apply finy at c
-      linarith
+      let I : Finset ℕ := P.filter (fun p ↦ f p < f n)
+      -- nota: esto no son filtros jaja
 
-      have hn1 : ∃ t ∈ P, f t < f n
+      have hI : I.Nonempty
       use 1
+      rw [Finset.mem_filter]
       constructor
       · exact h1
       · rw [hf1]
         specialize hf n
-        exact lt_of_le_of_ne hf.left (id (Ne.symm aux))
+        cases' hf with hf _
+        apply Ne.lt_of_le
+        rw [← hf1]
+        by_contra c
+        apply finy at c
+        linarith
+        exact hf
 
+      have hIP : I ⊆ P
+      exact Finset.filter_subset (fun p ↦ f p < f n) P
 
-      have hn2 : ∃ s ∈ P, f n < f s
-      use 0
+      let hImem : ∀ i ∈ I, i ∈ P ∧ f i < f n
+      intro i hi
+      exact Finset.mem_filter.mp hi
+
+      let i : ℕ := Finset.max' I hI
+
+      #check Finset.le_max'
+
+      have himax : ∀ m ∈ I, m ≤ i
+      exact fun m a ↦ Finset.le_max' I m a
+
+      right
+      right
+      use i
+
       constructor
-      · exact h0
-      · rw [hf0]
-        specialize hf n
-        exact lt_of_le_of_ne hf.right aux'
+      · apply hIP
+        exact Finset.max'_mem I hI
 
+      · constructor
 
+        · specialize hImem i (by exact Finset.max'_mem I hI)
+          exact hImem.right
 
+        · intro m hmP hm
+          -- ok aquí me he dado cuenta de que la he liado creo...
 
-      sorry
+          sorry
