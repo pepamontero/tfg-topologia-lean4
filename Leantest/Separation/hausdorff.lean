d19d786a00bcc98f579db0b4a94c5ac74817b83a
pepsi <pepamonterojimena@gmail.com>
2025-03-04 19:05:04 +0100

    move A_open_iff_is_heighbourhood_of_all from hausdorff.lean to open.lean

--- Leantest/Separation/hausdorff.lean
+++ Leantest/Separation/hausdorff.lean
@@ -1,4 +1,5 @@
 import Leantest.BasicProp.basic
+import Leantest.BasicProp.open
 import Leantest.TopoSpaces.discrete
 import Leantest.TopoSpaces.trivial
 import Leantest.TopoSpaces.usual
@@ -123,70 +124,6 @@ RESULTADO
 Si X es un espacio topológico Hausdorff,
 entonces todo conjunto unipuntual {x} es cerrado
 -/
-
-
-
-lemma A_open_iff_is_heighbourhood_of_all
-    {X : Type} [T : TopologicalSpace X]
-    (A : Set X) : IsOpen A ↔
-    ∀ x ∈ A, Neighbourhood A x := by
-  constructor
-  all_goals intro h
-  · -- →
-    intro x hx
-    use A
-    constructor
-    trivial
-    constructor
-    exact hx
-    exact h
-
-  · -- ←
-    -- classeical choose?
-    have h : ∀ a : X, a ∈ A → ∃ Ua : Set X, (Ua ⊆ A ∧ OpenNeighbourhood Ua a)
-    · exact h
-
-    have aux : A = ∅ ∨ ¬ (A = ∅)
-    exact Classical.em (A = ∅)
-
-    cases' aux with h1 h2
-
-    · -- A is empty
-      rw [h1]
-      exact isOpen_empty
-
-    · -- A is not empty
-      have hA : ∃ a, a ∈ A
-      · rw [← Set.not_nonempty_iff_eq_empty] at h2
-        simp at h2
-        exact h2
-
-      --let g : A → Set X := fun a : A ↦ Classical.choose (h a a.property)
-
-      have hUnion : A = ⋃ a : A, Classical.choose (h a a.property)
-      · ext x ; constructor <;> intro hx
-        · let hx' := Classical.choose_spec (h x hx)
-          simp
-          use x
-          use hx
-          exact hx'.right.left
-        · simp at hx
-          cases' hx with y hy
-          cases' hy with hy hx
-          let m := Classical.choose_spec (h y hy)
-          apply m.left
-          exact hx
-
-      rw [hUnion]
-
-      have hOpen : ∀ a : A, IsOpen (Classical.choose (h a a.property))
-      · intro a
-        let ha := Classical.choose_spec (h a a.property)
-        exact ha.right.right
-
-      exact isOpen_iUnion hOpen
-
-
 
 
 theorem Hausdorff_imp_singletones_closed {X : Type} [T : TopologicalSpace X]
