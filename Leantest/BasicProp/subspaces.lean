859fd1627e43b625f5eb4454359662171cd0b6c7
pepsi <pepamonterojimena@gmail.com>
2025-03-04 17:23:47 +0100

    move ico_open_in_Icc01 from normal.lean to subspaces.lean

--- Leantest/BasicProp/subspaces.lean
+++ Leantest/BasicProp/subspaces.lean
@@ -1,6 +1,7 @@
 
 
 import Leantest.BasicProp.basic
+import Leantest.TopoSpaces.usual
 
 example (X : Type) (A B C : Set X) : (A ∪ B) ∩ C = (A ∩ C) ∪ (B ∩ C) := by
   exact Set.union_inter_distrib_right A B C
@@ -82,3 +83,85 @@ def TopoSubspace {X : Type} (T : TopologicalSpace X) (Y : Set X) :
         · exact ht
         · use hx
           exact Set.mem_of_mem_image_val aux
+
+
+/-
+Example of use: prove that [0, b) is open for b < 1
+in [0, 1] seen as a topological subspace of ℝ with the usual topology
+-/
+
+lemma ico_open_in_Icc01 {Y : Set ℝ}
+    {hY : Y = Set.Icc 0 1}
+    {R : TopologicalSpace Y}
+    {hR : R = TopoSubspace UsualTopology Y}
+    (b : ℝ) (hb : 0 < b ∧ b < 1) :
+    R.IsOpen ({y | (y : ℝ) ∈ Set.Ico 0 b} : Set Y) := by
+
+  rw [hR] -- usar la topo. del subesp.
+  rw [UsualTopology] -- usar la def. de T_u
+  use ((Set.Ioo (-1) b) : Set ℝ)
+  constructor
+  · exact ioo_open_in_R (-1) b
+  · ext x
+    constructor
+    all_goals intro hx
+    · simp
+      simp at hx
+      cases' hx with hx1 hx2
+      constructor
+      · rw [hY] at hx2
+        simp at hx2
+        cases' hx2 with hx2 hx3
+        constructor
+        · linarith
+        · simp at hx3
+          linarith
+      · exact hx2
+    · simp
+      simp at hx
+      cases' hx with hx1 hx2
+      constructor
+      · rw [hY] at hx2
+        simp at hx2
+        constructor
+        · linarith
+        · linarith
+      · exact hx2
+
+lemma ioc_open_in_Icc01 {Y : Set ℝ}
+    {hY : Y = Set.Icc 0 1}
+    {R : TopologicalSpace Y}
+    {hR : R = TopoSubspace UsualTopology Y}
+    (b : ℝ) (hb2 : 0 < b ∧ b < 1) :
+    R.IsOpen ({y | (y : ℝ) ∈ Set.Ioc b 1} : Set Y) := by
+
+  rw [hR] -- usar la topo. del subesp.
+  rw [UsualTopology] -- usar la def. de T_u
+  use ((Set.Ioo b 2) : Set ℝ)
+  constructor
+  · exact ioo_open_in_R b 2
+  · ext x
+    constructor
+    all_goals intro hx
+    · simp
+      simp at hx
+      cases' hx with hx1 hx2
+      constructor
+      · rw [hY] at hx2
+        simp at hx2
+        cases' hx2 with hx2 hx3
+        constructor
+        · linarith
+        · simp at hx3
+          linarith
+      · exact hx2
+    · simp
+      simp at hx
+      cases' hx with hx1 hx2
+      constructor
+      · rw [hY] at hx2
+        simp at hx2
+        constructor
+        · linarith
+        · linarith
+      · exact hx2
