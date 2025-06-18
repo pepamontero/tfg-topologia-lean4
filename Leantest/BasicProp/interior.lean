faa8ef3756eb46d6352ff647e6127b8c2046ac1f
pepsi <pepamonterojimena@gmail.com>
2025-06-18 19:16:14 +0200

    unificaciones

--- Leantest/BasicProp/interior.lean
+++ Leantest/BasicProp/interior.lean
@@ -3,49 +3,39 @@ import Leantest.BasicProp.open
 
 -- DEFINICIÓN DE INTERIOR DE UN CONJUNTO
 #check interior
-def Interior {X : Type} [T : TopologicalSpace X] (A : Set X) : Set X :=
+def Interior {X : Type} [TopologicalSpace X] (A : Set X) : Set X :=
     {x : X | ∃ U : Set X, OpenNeighbourhood U x ∧ U ⊆ A}
 
 
 #check isOpen_interior
 -- EL INTERIOR DE UN CONJUNTO ES UN ABIERTO
-lemma interior_is_open {X : Type} [T : TopologicalSpace X] (A : Set X) :
-    IsOpen (Interior A) := by
-
+example {X : Type} [T : TopologicalSpace X]
+    (A : Set X) : IsOpen (interior A) := by
   apply A_open_iff_neighbourhood_of_all.mpr
-  simp [Neighbourhood]
-
-  intro x hx
-  rw [Interior] at hx
-  simp at hx
-  cases' hx with U hU
+  intro a ha
+  obtain ⟨U, hU, ha⟩ := ha
   use U
   constructor
-  · intro y hy
-    rw [Interior]
-    simp
+  · intro x hx
     use U
-    constructor
-    · constructor
-      · exact hy
-      · exact hU.left.right
-    · exact hU.right
-  · exact hU.left
+  · constructor
+    · exact ha
+    · exact hU.left
 
 #check interior_subset
 example {X : Type} [T : TopologicalSpace X] (A : Set X) :
     interior A ⊆ A := by
   intro a ha
-  obtain ⟨U, hU⟩ := ha
-  apply hU.left.right
-  exact hU.right
+  obtain ⟨U, hU, ha⟩ := ha
+  apply hU.right
+  exact ha
 
 #check interior_eq_iff_isOpen
 example {X : Type} [T : TopologicalSpace X] (A : Set X) :
     IsOpen A ↔ interior A = A:= by
-
-  constructor
-  all_goals intro h
+  constructor; swap; all_goals intro h
+  · rw [← h]
+    exact isOpen_interior
   · apply Set.Subset.antisymm
     · exact interior_subset
     · intro a ha
@@ -54,6 +44,3 @@ example {X : Type} [T : TopologicalSpace X] (A : Set X) :
       · simp
         exact h
       · exact ha
-
-  · rw [← h]
-    exact isOpen_interior
