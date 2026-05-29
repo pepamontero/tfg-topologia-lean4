68b694b831b0a741442e420b2ad7f7800311873c
pepamontero <pepamonterojimena@gmail.com>
2026-05-29 13:37:37 +0200

    simplify, start editing

--- docs/urysohn-docs/manual/Docs.lean
+++ docs/urysohn-docs/manual/Docs.lean
@@ -1,8 +1,4 @@
-/-
-Copyright (c) 2024-2025 Lean FRO LLC. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Author: David Thrane Christiansen
--/
+
 
 import VersoManual
 
@@ -33,23 +29,19 @@ set_option verso.exampleProject "../zippers"
 -- `(module := ...)` argument to most elements that show code.
 set_option verso.exampleModule "Zippers"
 
-#doc (Manual) "Zippers: A Documentation Example" =>
+#doc (Manual) "Formalización de las matemáticas con
+Lean. Resultados de
+Topología General y Lema de Urysohn." =>
 %%%
-authors := ["David Thrane Christiansen"]
-shortTitle := "Zippers"
+authors := ["Pepa Montero"]
+shortTitle := "UrysohnsLemma"
 %%%
 
-{index}[example]
-Here's an example project showing how to document a Lean package with Verso.
-It's a good idea to read the document's source together with the rendered output, because it demonstrates how to use various features.
+Este trabajo consiste en una exploración del sistema Lean 4 como asistente de demostración, mediante un caso de estudio: la formalización de resultados de topología general.
 
-This document's setup has the following properties:
- * It decouples the version of Lean used for Verso from that used by the library. This is important because it allows a library to stay on an older version of Lean while still adopting improvements to Verso that require new compiler versions.
- * The example code may use any Lean syntax, including custom syntax, and it will be highlighted correctly.
- * Examples to be included in the documentation are indicated with special comments.
+El objetivo principal es profundizar en el aprendizaje de este sistema y obtener una visión crítica de las ventajas y los inconvenientes que presenta al escribir matemáticas. Se presenta un acercamiento general a la base fundacional del sistema, su sintaxis y su aplicación concreta a la topología.
 
-The library here was chosen to be small.
-It is not intended as a realistic library, but rather to exercise certain interesting features.
+Finalmente, se detalla el proceso de formalización de un resultado complejo como es el Lema de Urysohn, junto con las conclusiones obtenidas del trabajo realizado.
 
 # Zippers
 
@@ -152,72 +144,3 @@ instance : Zipper (List α) Dir (ListZipper α) where
 ```
 
 :::
-
-{include 1 Docs.DocFeatures}
-
-# Notes
-
-Use {lean}`margin` to create a marginal note.{margin}[Marginal notes should be used like footnotes.]
-
-# Citations
-
-Cite works using {lean}`citet`, {lean}`citep`, or {lean}`citehere`.
-They take a name of a citable reference value as a parameter.
-References should be defined as values, typically in one module that is imported (similar to the role of a `.bib` file in LaTeX).
-
-Textual citations, as with {citet someThesis}[], look like this.
-Parenthetical {citep someArXiv}[] looks like this.
-Use {lean}`citehere` to literally include the cite rather than making a margin note, e.g. {citehere somePaper}[].
-Literally-included cites are mostly useful when performing citation inside a margin note.
-
-# Section References
-%%%
-tag := "sec-ref"
-%%%
-
-Sections with tags can be cross-referenced.
-They additionally gain permalink indicators that can be used to link to them even if the document is reorganized.
-Tags are added in section metadata, e.g.
-```
-%%%
-tag := "my-tag"
-%%%
-```
-They can be linked to using {lean}`ref`.
-Here's one to {ref "sec-ref"}[this section].
-
-
-
-# Viewing the Output
-
-Verso's HTML doesn't presently work correctly when opened directly in a browser, so it should be served via a server.{margin}[This is due to security restrictions on retrieved files: some of the code hovers are deduplicated to a JSON file that's fetched on demand.]
-Python has a simple web server module that's useful for this; however, it tends to send cache control headers that prevent some content from being refreshed.
-
-In the root of this source repository, there is a `serve.py` script that sends more appropriate headers.
-To use it, run `python3 serve.py 8000 -d DIR` to serve the files in `DIR` on `http://localhost:8000`.
-The port and root can be customized by modifying the appropriate command-line options.
-
-# Using an Index
-
-{index}[index]
-The index should contain an entry for “lorem ipsum”.
-{index}[lorem ipsum]
-{index (subterm := "of lorem")}[ipsum]
-{index (subterm := "per se")}[ipsum]
-{index}[ipsum]
-Lorem ipsum dolor {index}[dolor] sit amet, consectetur adipiscing elit, sed {index}[sed] do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris {index}[laboris] {see "lorem ipsum"}[laboris] {seeAlso "dolor"}[laboris] nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-This is done using the `{index}[term]` syntax. Sub-terms {index (subterm := "sub-term")}[entry] can be added using the `subterm` parameter to `index`.
-
-Multiple index {index}[index] targets for a term also work.
-
-{ref "index"}[Index link]
-
-
-# Index
-%%%
-number := false
-tag := "index"
-%%%
-
-{theIndex}
